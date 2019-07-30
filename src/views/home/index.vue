72deb5791cb8377e5a8ce1bfeffcea45d8878506
czc <zicheng.chen@zhiyoubao.com>
2019-07-30 13:26:07 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="pc-home">
+    <div class="pc-header clearfix">
+      <div class="box">
+        <ul class="clearfix">
+          <li>
+            <img
+              class="logo-img"
+              src="https://statics.lotsmall.cn/image/20190420/20190420085850itoxef.jpeg?imageMogr2/thumbnail/640x/strip/quality/50"
+              alt
+            />
+          </li>
+          <li class="active">
+            <router-link tag="span" :to="{ name: 'Home'}">首页</router-link>
+          </li>
+          <li>
+            <router-link tag="span" :to="{ name: 'Home'}">博客</router-link>
+          </li>
+          <li>
+            <router-link tag="span" :to="{ name: 'Home'}">博客</router-link>
+          </li>
+        </ul>
+        <div class="userInfo">
+          <img
+            src="https://statics.lotsmall.cn/image/20190420/20190420085850itoxef.jpeg?imageMogr2/thumbnail/640x/strip/quality/50"
+            alt
+          />
+          <i class="iconfont iconshangjiantou"></i>
+          <div class="select-box">
+            <div>11</div>
+            <div>11</div>
+            <div>11</div>
+            <div>11</div>
+          </div>
+        </div>
+      </div>
+    </div>
+     <router-view />
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.clearfix:after {
+  content: ' ';
+  display: block;
+  clear: both;
+  height: 0;
+}
+.fl{
+  float: left;
+}
+.fr{
+  float:right;
+}
+.pc-home {
+  background: #f5f6f7;
+  height: 100%;
+}
+.pc-header {
+  background: #ffffff;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.05);
+  .box {
+    width: 1200px;
+    margin: 0 auto;
+    .userInfo {
+      float: right;
+      height: 40px;
+      line-height: 38px;
+      position: relative;
+      .select-box {
+        display: none;
+        position: absolute;
+        top: 33px;
+        right: 0;
+        z-index: 99999;
+        background-color: #fff;
+        border-radius: 0 0 4px 4px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+        width: 124px;
+        padding: 8px 0;
+        box-sizing: border-box;
+      }
+      img {
+        height: 22px;
+        width: 22px;
+        border-radius: 50%;
+        margin-top: 9px;
+        float: left;
+      }
+      .iconshangjiantou {
+        font-size: 12px;
+        float: left;
+        padding: 0 5px;
+        padding-left: 5px;
+        cursor: pointer;
+        transition: all 0.5s;
+        transform-origin: 50% 50%;
+        // &:hover {
+        //   transform: rotate(180deg);
+        //   transform-origin: 50% 50%;
+        // }
+      }
+      &:hover {
+        .select-box {
+          display: block;
+        }
+        .iconshangjiantou {
+          transform: rotate(180deg);
+          transform-origin: 50% 50%;
+        }
+      }
+    }
+    ul {
+      float: left;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      li {
+        font-size: 14px;
+        color: #3d3d3d;
+        float: left;
+        .logo-img {
+          height: 40px;
+          width: 80px;
+          display: block;
+          margin-right: 10px;
+        }
+        span {
+          margin-right: 6px;
+          padding: 0 3px;
+          height: 40px;
+          display: block;
+          line-height: 38px;
+        }
+        &.active {
+          span {
+            position: relative;
+            &::after {
+              content: '';
+              position: absolute;
+              left: 0;
+              bottom: 0;
+              height: 2px;
+              width: 100%;
+              background: #ca0c16;
+            }
+          }
+        }
+        &:hover {
+          color: #ca0c16;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/home/tab.vue
@@ -0,0 +1,244 @@
+<template>
+  <div class="pc-home-tab1">
+    <div class="content-warper clearfix">
+      <div :class="{fixed:fixed}" class="nav">
+        <div class="fsdf" v-for="(item,index) in 20" :key="index">{{index}}{{fixed}}</div>
+      </div>
+      <div class="pc-content clearfix">
+        <div class="fl content-l">
+          <div class="question-item" v-for="(item,index) in arr" :key="index">
+            <div class="title clearfix">
+              <span class="fl title-text">阿萨大大十大打撒阿斯顿</span>
+              <span class="fr close">x</span>
+            </div>
+            <div
+              class="text"
+            >杂谈：我什么开始写博客，我又不是什么大犇？ 我写博客只是自己学习总结的一种方式 首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都首先我很多遇见的问题都</div>
+            <div class="user-info clearfix">
+              <dl class="clearfix fl">
+                <dd>
+                  <img
+                    src="https://statics.lotsmall.cn/image/20190420/20190420085850itoxef.jpeg?imageMogr2/thumbnail/640x/strip/quality/50"
+                    alt
+                  />
+                </dd>
+                <dd>
+                  <span>asdad</span>
+                </dd>
+                <dd>asdadsa</dd>
+                <dd>7天前</dd>
+              </dl>
+              <div class="fr num-info">
+                <div class="fl num-detai reader-num">
+                  <span class="text fl">阅读数</span>
+                  <span class="num fl">111</span>
+                </div>
+                <div class="fl num-detai discuss-num">
+                  <span class="text fl">评论数</span>
+                  <span class="num fl">111</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="content-l">asdada</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      arr: [1, 2, 3, 4, 5, 6, 7, 78, 8, 9, 90, 0, 0, 0]
+    }
+  },
+  mounted() {
+    let _this = this
+    function getScrollTop() {
+      var scrollTop = 0,
+        bodyScrollTop = 0,
+        documentScrollTop = 0
+      if (document.body) {
+        bodyScrollTop = document.body.scrollTop
+      }
+      if (document.documentElement) {
+        documentScrollTop = document.documentElement.scrollTop
+      }
+      scrollTop = bodyScrollTop - documentScrollTop > 0 ? bodyScrollTop : documentScrollTop
+      console.log(scrollTop)
+      if (scrollTop > 40) {
+        _this.fixed = true
+      } else {
+        _this.fixed = false
+      }
+      return scrollTop
+    }
+
+    //文档的总高度
+
+    function getScrollHeight() {
+      var scrollHeight = 0,
+        bodyScrollHeight = 0,
+        documentScrollHeight = 0
+      if (document.body) {
+        bodyScrollHeight = document.body.scrollHeight
+      }
+      if (document.documentElement) {
+        documentScrollHeight = document.documentElement.scrollHeight
+      }
+      scrollHeight = bodyScrollHeight - documentScrollHeight > 0 ? bodyScrollHeight : documentScrollHeight
+      return scrollHeight
+    }
+
+    //浏览器视口的高度
+
+    function getWindowHeight() {
+      var windowHeight = 0
+      if (document.compatMode == 'CSS1Compat') {
+        windowHeight = document.documentElement.clientHeight
+      } else {
+        windowHeight = document.body.clientHeight
+      }
+      return windowHeight
+    }
+
+    window.onscroll = function() {
+      if (getScrollTop() + getWindowHeight() == getScrollHeight()) {
+        _this.arr = _this.arr.concat([1, 2, 3, 4, 5, 6, 76, 7, 8, 9])
+      }
+    }
+  }
+}
+</script>
+<style lang="scss">
+.pc-home-tab1 {
+  background: #f5f6f7;
+  .pc-content {
+    .content-l {
+      width: 760px;
+      min-height: 500px;
+      margin-right: 12px;
+      .question-item {
+        background: #fff;
+        padding: 5px 15px;
+        margin-bottom: 2px;
+        &:hover {
+          background: #fafafa;
+        }
+        .user-info {
+          .num-info {
+            .reader-num {
+              padding-right: 5px;
+              position: relative;
+              &::after {
+                position: absolute;
+                content: '';
+                height: 15px;
+                width: 1px;
+                top: 4px;
+                right: 0;
+                background: #e0e0e0;
+              }
+            }
+            .discuss-num {
+              margin-left: 5px;
+            }
+            .num-detai {
+              .text {
+                color: #8a8a8a;
+                margin-right: 6px;
+              }
+              .num {
+                color: #3399ea;
+              }
+              &:hover {
+                .text {
+                  color: #3399ea;
+                }
+              }
+            }
+          }
+          dl {
+            padding: 0;
+            margin: 0;
+            dd {
+              float: left;
+              padding: 0 7px;
+              position: relative;
+              &::after {
+                position: absolute;
+                content: '';
+                height: 15px;
+                width: 1px;
+                background: #e0e0e0;
+                top: 4px;
+                right: 0;
+              }
+              img {
+                height: 24px;
+                width: 24px;
+                border-radius: 50%;
+              }
+            }
+          }
+        }
+        .text {
+          margin-bottom: 4px;
+          color: #8a8a8a;
+          font-size: 14px;
+          line-height: 24px;
+          word-break: break-all;
+          display: -webkit-box;
+          -webkit-line-clamp: 1;
+          /* ! autoprefixer: off */
+          -webkit-box-orient: vertical;
+          /* autoprefixer: on */
+          overflow: hidden;
+        }
+        .title {
+          .title-text {
+            color: #3d3d3d;
+            font-size: 18px;
+            font-weight: 600;
+          }
+          .close {
+            font-size: 16px;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+    .content-r {
+      width: 300px;
+      display: block;
+    }
+  }
+  .content-warper {
+    width: 1180px;
+    padding: 12px 0 0;
+    margin: 0 auto;
+    .nav {
+      float: left;
+      width: 96px;
+      background-color: #fff;
+      margin-right: 12px;
+      text-align: center;
+      line-height: 32px;
+      position: relative;
+      padding: 6px 0;
+      -webkit-box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.04);
+      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.04);
+      &.fixed {
+        position: fixed;
+        top: 0;
+      }
+    }
+    .pc-content {
+      width: 1072px;
+      float: right;
+    }
+  }
+}
+</style>
